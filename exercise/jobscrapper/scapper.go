09604b48319fbacd9da12b2ebde9004eee09526f
wonjoon <39115630+wnjoon@users.noreply.github.com>
2021-01-28 22:42:28 +0900

    create webserver using goecho

--- exercise/jobscrapper/scapper.go
+++ exercise/jobscrapper/scapper.go
@@ -1,4 +1,4 @@
-package main
+package scrapper
 
 import (
 	"encoding/csv"
@@ -13,7 +13,6 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-var baseURL string = "https://kr.indeed.com/jobs?q=python"
 var jobPerPage int = 10
 
 type jobInfo struct {
@@ -24,19 +23,21 @@ type jobInfo struct {
 	summary  string
 }
 
-func main() {
+// Scrape indeed by term
+func Scrape(term string) {
 
 	defer elapsedTime("Scrapper Test", "start")()
+	var baseURL string = "https://kr.indeed.com/jobs?q=" + term
 
 	var jobs []jobInfo
 
 	c := make(chan []jobInfo)
 
-	totalPages := getPageNumber()
+	totalPages := getPageNumber(baseURL)
 
 	for i := 0; i < totalPages; i++ {
 		// combine all slices into one slice
-		go getPageInfo(i, c)
+		go getPageInfo(i, baseURL, c)
 		// extractJobs...: if ... -> [x]+[x] => [xx] / else [x] + [x] -> [[x][x]] (... = content appending)
 	}
 
@@ -49,11 +50,8 @@ func main() {
 	fmt.Println("Done, extracted", len(jobs))
 }
 
-/*
- * getPageNumber
- * - Get number of pages about searched job
- */
-func getPageNumber() int {
+// Get number of pages about searched job
+func getPageNumber(baseURL string) int {
 
 	pageNumber := 0
 
@@ -74,11 +72,8 @@ func getPageNumber() int {
 	return pageNumber
 }
 
-/*
- * getPageInfo
- * - Get information about jobs per page
- */
-func getPageInfo(page int, mainC chan<- []jobInfo) {
+// Get information about jobs per page
+func getPageInfo(page int, baseURL string, mainC chan<- []jobInfo) {
 
 	var jobs []jobInfo
 	c := make(chan jobInfo)
@@ -110,13 +105,10 @@ func getPageInfo(page int, mainC chan<- []jobInfo) {
 	mainC <- jobs
 }
 
-/*
- * writeJobs
- * -  Write searched information to csv
- */
+// Write searched information to csv
 func writeJobs(jobs []jobInfo) {
 	// csv library (make excel)
-	file, err := os.Create("jobs.csv")
+	file, err := os.Create("../jobscrapper/jobs.csv")
 	checkError(err)
 
 	c := make(chan []string)
@@ -143,66 +135,47 @@ func writeJobs(jobs []jobInfo) {
 	}
 }
 
-/*
- * writeJob
- * - Homework to adjust goroutine to writejobs for distributing workload using seperate execution
- */
+// [!] Homework to adjust goroutine to writejobs for distributing workload using seperate execution
 func writeJob(job jobInfo, c chan []string) {
 	applyLink := "https://kr.indeed.com/viewjob?jk="
 	jobSlice := []string{applyLink + job.id, job.title, job.location, job.salary, job.summary}
 	c <- jobSlice
 }
 
-/*
- * extractJobCard
- * - Extract information from job card
- * - Set variables in jobInfo structure
- */
+// Extract information from job card, set variables in jobInfo structure
 func extractJobCard(card *goquery.Selection, c chan<- jobInfo) {
 	id, _ := card.Attr("data-jk") // can not initialize variable with two values
-	title := cleanString(card.Find(".title>a").Text())
-	location := cleanString(card.Find(".sjcl").Text())
-	salary := cleanString(card.Find(".salaryText").Text())
-	summary := cleanString(card.Find(".summary").Text())
+	title := CleanString(card.Find(".title>a").Text())
+	location := CleanString(card.Find(".sjcl").Text())
+	salary := CleanString(card.Find(".salaryText").Text())
+	summary := CleanString(card.Find(".summary").Text())
 
 	c <- jobInfo{id: id, title: title, location: location, salary: salary, summary: summary}
 }
 
-/*
- * checkError
- * - Check error from http.Get
- */
+// Check error from http.Get
 func checkError(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-/*
- * checkResponseCode
- * - Check response code from http.Get
- */
+// Check response code from http.Get
 func checkResponseCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status", res.StatusCode)
 	}
 }
 
-/*
- * cleanString
- * - Clean blanks from result and get pure string
- * - strings.TrimSpace: spaces will be deleted by TrimSpace(front to end)
- * - strings.Fields: seperate all words using space -> remove all tiny spaces and just put 'pure' words into array
- * - strings.Join: "hello       9    f" -> "hello 9 f" (when using seperator " ")
- */
-func cleanString(str string) string {
+// CleanString clean blanks from result and get pure string
+func CleanString(str string) string {
+	// strings.TrimSpace: spaces will be deleted by TrimSpace(front to end)
+	// strings.Fields: seperate all words using space -> remove all tiny spaces and just put 'pure' words into array
+	// strings.Join: "hello       9    f" -> "hello 9 f" (when using seperator " ")
 	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
-/*
- * elapsedTime
- * - To get a time
- */
+// To get a time
 func elapsedTime(tag string, msg string) func() {
 	if msg != "" {
 		log.Printf("[%s] %s", tag, msg)
--- exercise/webserver/main.go
+++ exercise/webserver/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"strings"
+
+	scrapper "../jobscrapper"
+	"github.com/labstack/echo"
+)
+
+// You should remove /v4 to get a 'go echo'
+// go get github.com/labstack/echo
+
+const fileName string = "../jobscrapper/jobs.csv"
 
 func main() {
-	fmt.Println("HELLO")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
+
+}
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, "jobs.csv")
 }
